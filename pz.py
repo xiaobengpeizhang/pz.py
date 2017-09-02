f0308b255aecde8a040e61841c5fa48dafe025f6
xiaobengpeizhang <peizhang@aliyun.com>
2017-09-02 20:44:52 +0800

    添加函数 绘制任意高的树

--- pz.py
+++ pz.py
@@ -5,3 +5,28 @@ def find_location (fullString, search) :
     c[0] = a
     c[1] = b
     return c
+
+def draw_tree():
+    x = 0
+    while (x < 2):
+        x = int(input('请输入你想绘制树的高度，大于1的任意整数：'))
+    # print(x)
+    # x * 2 - 1
+    # no_odd = [1, 3, 5, 7, 9, 11, 13, 15, 17]
+    sum_char = x * 2 - 1
+    # print(sum_char)
+    line = 1
+    while (line <= x):
+        left = (sum_char - line - line + 1) / 2
+        print(int(left) * ' ', end='')
+        star = 1
+        while (star <= (sum_char - left*2)):
+            if((star+1)%2==0):
+                print('*', end='')
+            else:
+                print(' ', end='')
+            star+=1
+        print(int(left)*' ', end='')
+        line += 1
+        print('')
+    print(int((sum_char - 1) / 2) * ' ' + '|' + int((sum_char - 1) / 2) * ' ',end='')
